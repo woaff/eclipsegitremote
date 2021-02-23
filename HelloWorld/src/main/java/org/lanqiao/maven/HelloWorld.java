ae91bb0bf6ac42fa14c7130d3f8af59b9356c9fa
woaff <447385206@qq.com>
2021-02-23 16:58:08 +0800

    解决冲突

--- HelloWorld/src/main/java/org/lanqiao/maven/HelloWorld.java
+++ HelloWorld/src/main/java/org/lanqiao/maven/HelloWorld.java
@@ -1,10 +1,6 @@
 package org.lanqiao.maven;
 public class HelloWorld{
 	public String sayHello(String name){
-<<<<<<< HEAD
-		return "Hello "+name;//hello b
-=======
-		return "Hello "+name;//hello a
->>>>>>> branch 'master' of https://github.com/woaff/eclipsegitremote.git
+		return "Hello "+name;//hello b+a
 	}
 }
